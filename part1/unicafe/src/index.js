5b07524a2c061f7637d1dc79fe4cccb4be556519
jhonf <jhonfuto19@gmail.com>
2021-09-19 17:04:24 -0500

    Step 1: buttons and statistics

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Title = ({content}) => <h1>{content}</h1>
+
+const Button = ({text, handleOnClick}) =>(
+  <button onClick={handleOnClick}>{text}</button>
+)
+
+const Content = ({content}) => <p>{content[0]} {content[1]}</p>
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Title content='give feedback'/>
+      <Button handleOnClick={() => setGood(good+1)} text='good'/>
+      <Button handleOnClick={() => setNeutral(neutral+1)} text='neutral'/>
+      <Button handleOnClick={() => setBad(bad+1)} text='bad'/>
+      <Content content={['good',good]}/>
+      <Content content={['neutral',neutral]}/>
+      <Content content={['bad',bad]}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
