6713c61154165aae5f6f6282a9a58c18bb97d6ef
jhonf <jhonfuto19@gmail.com>
2021-09-19 22:04:14 -0500

    Part4: Component satatistics

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,15 +9,24 @@ const Button = ({text, handleOnClick}) =>(
 
 const Content = ({content}) => <p>{content[0]} {content[1]}</p>
 
+const Statistics = ({good, neutral, bad}) =>{
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = String((good/all)*100)+'%'
+  return (
+    <div>
+      <Content content={['all',all]}/>
+      <Content content={['average',average]}/>
+      <Content content={['positive', positive]}/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = String((good/all)*100)+'%'
 
   return (
     <div>
@@ -28,9 +37,7 @@ const App = () => {
       <Content content={['good',good]}/>
       <Content content={['neutral',neutral]}/>
       <Content content={['bad',bad]}/>
-      <Content content={['all',all]}/>
-      <Content content={['average',average]}/>
-      <Content content={['positive', positive]}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
