c714d766ddcd15a5be5caf80d98bf55d9d4deeed
jhonf <jhonfuto19@gmail.com>
2021-09-20 10:21:13 -0500

    Step 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,25 +1,45 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({text, handleOnClick}) =>(
+const Tile = ({content}) => <h1>{content}</h1>
+
+const Button = ({text, handleOnClick}) => (
   <button onClick={handleOnClick}>{text}</button>
 )
+const Anecdote = ({anecdotes, points, selected}) => {
+  if  (selected === -1) {
+    return (
+      <div>
+        <p>None</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      {anecdotes[selected]}
+      <p>has {points[selected]} votes</p>
+    </div>
+  )
+}
 const random = (min, max) => {
-  return Math.floor((Math.random() * (max - min + 1)) + min);
+  return Math.floor((Math.random() * (max - min + 1)) + min)
 }
 
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
-  const [points, setPoints] = useState(INITIAL_STATE)
+  const [points, setPoints] = useState(INITIAL_STATE_POINTS)
+  const [most, setMost] = useState(-1)
 
   const incrementVote = () => {
     const newPoints = [...points]
     newPoints[selected] += 1
     setPoints(newPoints)
+    setMost(newPoints.indexOf(Math.max(...newPoints)))
   }
-
+  
   return (
     <div>
+      <Tile content='Anecdote of the day' />
       {anecdotes[selected]}
       <p>has {points[selected]} votes</p>
       <div>
@@ -27,6 +47,8 @@ const App = ({anecdotes}) => {
         <Button text='next anecdote' 
           handleOnClick={()=> setSelected(random(0,anecdotes.length-1))}/>
       </div>
+      <Tile content='Anecdote with most votes' />
+      <Anecdote anecdotes={anecdotes} points={points} selected={most} />
     </div>
   )
 }
@@ -40,7 +62,8 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const INITIAL_STATE = new Array(anecdotes.length).fill(0)
+const INITIAL_STATE_POINTS = new Array(anecdotes.length).fill(0)
+
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
