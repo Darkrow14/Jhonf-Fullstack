f3ace42b6faabbb5e3fbf2623a05b2037b720a37
jhonf <jhonfuto19@gmail.com>
2021-09-19 23:16:13 -0500

    Step 2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({text, handleOnClick}) =>(
-  <div>
-    <button onClick={handleOnClick}>{text}</button>
-  </div>
+  <button onClick={handleOnClick}>{text}</button>
 )
 const random = (min, max) => {
   return Math.floor((Math.random() * (max - min + 1)) + min);
@@ -12,12 +10,23 @@ const random = (min, max) => {
 
 const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(INITIAL_STATE)
+
+  const incrementVote = () => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
 
   return (
     <div>
       {anecdotes[selected]}
-      <Button text='next anecdote' 
-        handleOnClick={()=> setSelected(random(0,anecdotes.length-1))}/>
+      <p>has {points[selected]} votes</p>
+      <div>
+        <Button text='vote' handleOnClick={incrementVote} />
+        <Button text='next anecdote' 
+          handleOnClick={()=> setSelected(random(0,anecdotes.length-1))}/>
+      </div>
     </div>
   )
 }
@@ -30,6 +39,8 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+const INITIAL_STATE = new Array(anecdotes.length).fill(0)
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
